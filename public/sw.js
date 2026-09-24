583095a34334ffaed4bee4645d1711fa96691713
ashirbadkachchar-maker <ashirbadkachchar@gmail.com>
2026-09-24 15:38:36 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE = "paaniwala-v1";
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(["/", "/manifest.json"])).then(() => self.skipWaiting())
+  );
+});
+self.addEventListener("activate", (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request).then((r) => r || caches.match("/"))
+    )
+  );
+});
